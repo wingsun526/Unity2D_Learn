31d6edcba11828f609701384d7fb40415bc3707c
Marco Man <wingsun526@gmail.com>
2021-10-04 16:42:58 -0400

    Lesson 19

---
+++ Delivery Driver/Assets/Collision.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Collision");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("You just crossed that line!");
+        
+    }
+}
