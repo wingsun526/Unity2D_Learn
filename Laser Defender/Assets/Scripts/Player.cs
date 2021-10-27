19921207c654495ad88d7cf3d8a7acf3a99cc63a
Marco Man <wingsun526@gmail.com>
2021-10-27 17:32:15 -0400

    New Laser Defender Project

---
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector2 rawInput;
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 delta = rawInput * (moveSpeed * Time.deltaTime);
+        transform.position += delta;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
+    }
+}
