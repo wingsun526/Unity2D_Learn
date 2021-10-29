ca49f60eff1febb6513a77b2f0efa01a42cd025b
Marco Man <wingsun526@gmail.com>
2021-10-28 20:23:57 -0400

    Lessons 112

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,38 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     private Vector2 rawInput;
+    [SerializeField] private float paddingLeft;
+    [SerializeField] private float paddingRight;
+    [SerializeField] private float paddingTop;
+    [SerializeField] private float paddingBottom;
+    
+    private Vector2 minBounds;
+    private Vector2 maxBounds;
+    
+
+    private void Start()
+    {
+        InitBounds();
+    }
+
     void Update()
     {
         Move();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
     private void Move()
     {
-        Vector3 delta = rawInput * (moveSpeed * Time.deltaTime);
-        transform.position += delta;
+        Vector2 delta = rawInput * (moveSpeed * Time.deltaTime);
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
