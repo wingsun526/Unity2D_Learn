eb495bfbe9faa7ade1f7f9c1ae4df9017e04100e
Marco Man <wingsun526@gmail.com>
2021-10-29 17:23:46 -0400

    lessons 116

---
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private List<WaveConfigSO> waveConfigs;
+    [SerializeField] private float timeBetweenWaves = 0f;
+    WaveConfigSO currentWave;
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyWaves());
+    }
+
+    public WaveConfigSO GetCurrentWace()
+    {
+        return currentWave;
+    }
+    IEnumerator SpawnEnemyWaves()
+    {
+        foreach (WaveConfigSO wave in waveConfigs)
+        {
+            currentWave = wave;
+            for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+            {
+                Instantiate(currentWave.GetEnemyPrefab(i),
+                    currentWave.GetStartingWaypoint().position,
+                    Quaternion.identity,
+                    transform);  // call object on runtime
+                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+            }
+
+            yield return new WaitForSeconds(timeBetweenWaves);
+        }
+        
+        
+    }
+    
+}
