eddac42269d3bc69b5c9e42d0c24ce117c53bd81
Marco Man <wingsun526@gmail.com>
2021-11-02 14:14:13 -0400

    UI Lessons

--- Laser Defender/Assets/Scripts/ScoreKeeper.cs
+++ Laser Defender/Assets/Scripts/ScoreKeeper.cs
@@ -15,7 +15,7 @@ public class ScoreKeeper : MonoBehaviour
     {
         score += value;
         Mathf.Clamp(score, 0, int.MaxValue);
-        Debug.Log(score);
+        //Debug.Log(score);
     }
 
     public void ResetScore()
